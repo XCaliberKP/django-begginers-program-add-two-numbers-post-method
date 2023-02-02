a3b8bd8d4b972617b2063ef4da921ebd393d2f33
Khushal Pathak <khushalpathak@gmail.com>
2023-02-02 13:57:38 +0530

    Normal Commit

--- first/settings.py
+++ first/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/ 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-jx$b$q+)tet@vld_hqoi_ssmiw#9*4f+=t8e%nb9t2i4_^z@k8'
