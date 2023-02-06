49659627fb5ef4d6f4ea49ec338cb1ae8338bc39
Khushal Pathak <khushalpathak@gmail.com>
2023-02-06 12:54:39 +0530

    Django Class

--- calc/views.py
+++ calc/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello All, Welcome to Django Class")
+    return render(request, 'home.html')
