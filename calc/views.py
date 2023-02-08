191b474eafc8f4028f002a1786da61e029096c14
Khushal Pathak <khushalpathak@gmail.com>
2023-02-08 14:02:11 +0530

    Django Class

--- calc/views.py
+++ calc/views.py
@@ -4,5 +4,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'home.html', {'heading' : 'CALCULATOR (Adding Two Numbers)'})
+    return render(request, 'home.html', {'heading': 'CALCULATOR (Adding Two Numbers)'})
+
+
+def add(request):
+    val1 = int(request.GET['first'])
+    val2 = int(request.GET['second'])
+    res = val1 + val2
+    return render(request, 'result.html', {'result': res})
